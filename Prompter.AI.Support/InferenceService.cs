5f191a7a3e20eb77adcb33d123c802db9b9a1637
Sambeet Das <sambeet.gugul@gmail.com>
2024-07-21 13:19:02 +0530

    api implemented

---
+++ Prompter.AI.Support/InferenceService.cs
@@ -0,0 +1,26 @@
+﻿using Prompter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prompter.AI.Support
+{
+    public class InferenceService
+    {
+        public async Task<string> Execute(string input)
+        {
+            var config = InferenceModelBuilder.Build();
+
+            InferenceModule inferenceModule = new InferenceModule(config);
+
+            var infModel = new InferenceModel
+            {
+                UserPrompt = input,
+            };
+
+            return await inferenceModule.Processer(infModel);
+        }
+    }
+}
--- Prompter.AI.Support/Program.cs
+++ Prompter.AI.Support/Program.cs
@@ -1,79 +1,17 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Prompter.Models;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Prompter.AI.Support;
 
-namespace Prompter.Test
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<InferenceService>();
+
+var app = builder.Build();
+
+
+app.MapGet("/inference/{input}", async (string input, InferenceService service) =>
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            Console.WriteLine("Ask question:");
-            string input = Console.ReadLine();
+    return await service.Execute(input);
+});
 
-            if (!String.IsNullOrWhiteSpace(input))
-            {
-                var services = new ServiceCollection();
-                services.AddPrompterService();
-
-                var config = new InferenceConfigModel
-                {
-                    ModelProvider = ModelProviderEnum.HuggingFace,
-                    ModelName = "mistralai/Mistral-7B-Instruct-v0.3",
-                    AuthKey = "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",
-                    ToolModel = new ToolModel
-                    {
-                        APISource = new List<APISourceDetails>
-                    {
-                        new APISourceDetails
-                        {
-                            Id = "get_all_phones",
-                            Description = "This api is used to get the list of phones",
-                            Url = "https://api.restful-api.dev/objects",
-                            Method = "GET",
-                            Headers = new Dictionary<string, string>(),
-                            Request = string.Empty,
-                            ResponseKeys = new Dictionary<string, string>()
-                            {
-                                { "name", "name" },
-                                { "price","price" }
-                            }
-                        },
-                        new APISourceDetails
-                        {
-                            Id = "get_breeds_of_dogs",
-                            Description = "This api is used to get the list of breeds of dogs",
-                            Url = "https://dogapi.dog/api/v2/breeds",
-                            Method = "GET",
-                            Headers = new Dictionary<string, string>(),
-                            Request = string.Empty,
-                            ResponseKeys = new Dictionary<string, string>()
-                            {
-                                { "name", "name" },
-                                { "description","description" }
-                            }
-                        }
-                    }
-                    }
-                };
-
-
-
-                InferenceModule inferenceModule = new InferenceModule(config);
-
-                var infModel = new InferenceModel
-                {
-                    UserPrompt = input,
-                };
-
-                var response = await inferenceModule.Processer(infModel);
-
-                Console.WriteLine(response);
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("No question found. Re-run the application again.");
-            }
-        }
-    }
-}
+app.Run();
